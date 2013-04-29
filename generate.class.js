52ae0281d09889bff93c084bb3ed81b4e9c5a117
nmatei <matei.nick@gmail.com>
2013-04-29 15:31:40 +0300

    improve generate class js to use correct constructors

--- generate.class.js
+++ generate.class.js
@@ -1,46 +1,79 @@
 // TODO in EditorGridPanel add clickToEdit option new EditorGridPanel(this).setClicksToEdit(2)
+// TODO fieldLabel must have ":" have labelSeparator
 
+var logId = false;
+var elemCount = 0;
 function getVarName(name){
-    name = name ? name.replace(/\s/g, '') : 'name';
+    name = name ? name.replace(/\s/g, '') : 'item' + (elemCount++);
     name = name.replace(/[\(\)/\\:;\<>=\-"]/gi, '');
     name = Ext.util.Format.capitalize(name);
     return !name ? name : name.charAt(0).toLowerCase() + name.substr(1);
 }
 
+function getPanelConfig(item){
+    var code = '';
+    if(item.title){
+        code += '.setTitle("' + item.title + '")';
+    } else if(item.fieldLabel){
+        code += '.setLabel("' + item.fieldLabel + '")';
+    }
+    return code;
+}
+
 function getItemCode(container, item){
-    var code = '\tpublic ',
+    var name = '',
+        code = '\tpublic ',
         xtype = item.getXType(),
         xtypes = item.getXTypes(),
         label = item.fieldLabel;
 
     //console.debug('getItemCode', container, item);
     if(xtype == 'editorgrid' || xtypes.indexOf('/editorgrid/') != -1 || item instanceof Ext.grid.EditorGridPanel){
-        code += 'EditorGridPanel ' + getVarName(item.title) + 'EditorGridPanel = new EditorGridPanel(' + container + ', "' + (item.title || label) +  '")';
+        name = getVarName(item.title) + 'EditorGridPanel';
+        code += 'EditorGridPanel ' + name + ' = new EditorGridPanel(' + container + ')';
+        code += getPanelConfig(item);
     } else if(xtype == 'grid' || xtypes.indexOf('/grid/') != -1 || item instanceof Ext.grid.GridPanel){
-        code += 'GridPanel ' + getVarName(item.title) + 'GridPanel = new GridPanel(' + container + ', "' + item.title +  '")';
+        name = getVarName(item.title) + 'GridPanel';
+        code += 'GridPanel ' + name + ' = new GridPanel(' + container + ')';
+        code += getPanelConfig(item);
     } else if(xtype == 'panel' || xtypes.indexOf('/panel/') != -1 || item instanceof Ext.Panel){
-        code += 'Panel ' + getVarName(item.title) + 'Panel = new Panel(' + container + ', "' + item.title +  '")';
+        console.debug('panel item:', item.id, item);
+        name = getVarName(item.title) + 'Panel';
+        code += 'Panel ' + name + ' = new Panel(' + container + ')';
+        code += getPanelConfig(item);
     } else if(xtype == 'combo' || xtypes.indexOf('/combo/') != -1){
-        code += 'ComboBox ' + getVarName(label) + 'ComboBox = new ComboBox(' + container + ', "' + label +  '")';
+        name = getVarName(label) + 'ComboBox';
+        code += 'ComboBox ' + name + ' = new ComboBox(' + container + ', "' + label +  '")';
     } else if(xtype == 'textfield' || xtypes.indexOf('/textfield/') != -1){
-        code += 'TextField ' + getVarName(label) + 'TextField = new TextField(' + container + ', "'  + label +  '")';
+        name = getVarName(label) + 'TextField';
+        code += 'TextField ' + name + ' = new TextField(' + container + ', "'  + label +  '")';
     } else if(xtype == 'displayfield' || xtypes.indexOf('/displayfield/') != -1){
-        code += 'DisplayField ' + getVarName(label) + 'DisplayField = new DisplayField(' + container + ', "'  + label +  '")';
+        name = getVarName(label) + 'DisplayField';
+        code += 'DisplayField ' + name + ' = new DisplayField(' + container + ', "'  + label +  '")';
     } else if(xtype == 'checkbox' || xtypes.indexOf('/checkbox/') != -1){
-        code += 'Checkbox ' + getVarName(label) + 'Checkbox = new Checkbox(' + container + ', "'  + (label || item.boxLabel) +  '")';
+        name = getVarName(label || item.boxLabel) + 'Checkbox';
+        code += 'Checkbox ' + name + ' = new Checkbox(' + container + ', "'  + (label || item.boxLabel) +  '")';
     } else if(xtype == 'button' || xtypes.indexOf('/button/') != -1){
-        code += 'Button ' + getVarName(item.text) + 'Button = new Button(' + container + ', "'  + item.text +  '")';
+        name = getVarName(item.text) + 'Button';
+        code += 'Button ' + name + ' = new Button(' + container + ', "'  + item.text +  '")';
     } else if(!isNecesaryType(xtype, xtypes)){
         code = '';
     } else {
         //code += 'XYZ xyz = new XYZ("' + xtype + '", "' + xtypes + '")';
 		code = '';
-        console.warn(container, item, xtype, xtypes);
+        console.warn(container, item, xtype, xtypes, item.id);
     }
     if(code != ''){
-        code += ';\n';
+        code += ';';
+        if(logId){
+            code += '// ' + item.id;
+        }
+        code += '\n';
     }
-    return code;
+    return {
+        code: code,
+        name: name
+    };
 }
 
 function isNecesaryType(xtype, xtypes){
@@ -53,6 +86,10 @@ function isNecesaryType(xtype, xtypes){
         necesary = false;
     } else if(xtype == 'tbspacer' || xtypes.indexOf('/tbspacer/') != -1){
         necesary = false;
+    } else if(xtype == 'tbseparator' || xtypes.indexOf('/tbseparator/') != -1){
+        necesary = false;
+    } else if(xtype == 'box' || xtypes.indexOf('/box/') != -1){
+        necesary = false; // TODO this is temporary
     }
 	return necesary;
 }
@@ -70,10 +107,12 @@ function getEachElementsCode(container, items, isTab){
             }
         } else {
             items.each(function(it){
+                var c = container;
                 if(isTab){
-                    code += 'public TabPanel ' + getVarName(it.title) + 'TabPanel = new TabPanel(' + container + ', "' + it.title +  '");\n';
+                    c = getVarName(it.title) + 'TabPanel';
+                    code += '\tpublic TabPanel ' + c + ' = new TabPanel(' + container + ', "' + it.title +  '");\n';
                 }
-                code += getItemsCode(container, it);
+                code += getItemsCode(c, it);
             });
         }
     }
@@ -83,13 +122,16 @@ function getEachElementsCode(container, items, isTab){
 
 function getItemsCode(container, item){
     //console.debug('getItemsCode', container, item);
-    var code = '',
+    var elements,
+        code = '',
         tbar = item.getTopToolbar ? item.getTopToolbar() : false,
         bbar = item.getBottomToolbar ? item.getBottomToolbar(): false;
 
     var isTab = isTabPanel(item);
     if(!isTab){
-        code += getItemCode(container, item);
+        elements = getItemCode(container, item);
+        code += elements.code;
+        container = elements.name || container;
     }
     code += getEachElementsCode(container, item.items, isTab);
     if(tbar){
@@ -108,11 +150,12 @@ function getActiveWinClassCode(){
         code;
     console.debug('getActiveWinClassCode', name);
     code = 'public class ' + name + 'Window extends Window {\n';
-    code += ' public ' + name + 'Window(){\n setTitle("' + w.title + '");\n}\n';
+    code += '\tpublic ' + name + 'Window(){\n';
+    code += '\t\tsetTitle("' + w.title + '");\n\t}\n';
     w.items.each(function(it){
         code += getItemsCode('this', it);
     });
-    code += '}\n';
+    code += '\t}\n';
     return code;
 }
 
