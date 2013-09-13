0ee60d48201ecaf3789e85703d766b4437ab89b8
Viorel Culea <vculea@sdl.com>
2013-09-13 12:44:47 +0300

    comment CheckBoxTest.java

--- src/test/functional/java/com/sdl/weblocator/extjs/CheckBoxTest.java
+++ src/test/functional/java/com/sdl/weblocator/extjs/CheckBoxTest.java
@@ -1,17 +1,12 @@
 package com.sdl.weblocator.extjs;
 
-import com.extjs.selenium.form.Checkbox;
-import com.extjs.selenium.panel.Panel;
 import com.sdl.weblocator.TestBase;
 import org.apache.log4j.Logger;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
 
 public class CheckBoxTest extends TestBase {
     private static final Logger logger = Logger.getLogger(CheckBoxTest.class);
 
-    Panel simpleFormPanel = new Panel(null, "Simple Form");
+    /*Panel simpleFormPanel = new Panel(null, "Simple Form");
     Checkbox rightCheckBox = new Checkbox("CatRight", simpleFormPanel);
     Checkbox leftCheckBox = new Checkbox("CatLeft:", simpleFormPanel);
 
@@ -25,5 +20,5 @@ public class CheckBoxTest extends TestBase {
     public void leftCheckBox() {
         leftCheckBox.click();
         assertTrue(leftCheckBox.isSelected());
-    }
+    }*/
 }
