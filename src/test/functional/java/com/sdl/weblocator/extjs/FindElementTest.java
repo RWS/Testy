850decce9a9445f269bf6a30a6cc1c8f4a4250ca
Viorel Culea <vculea@sdl.com>
2013-09-27 14:57:13 +0300

    FindElementTest.java

--- src/test/functional/java/com/sdl/weblocator/extjs/FindElementTest.java
+++ src/test/functional/java/com/sdl/weblocator/extjs/FindElementTest.java
@@ -2,9 +2,12 @@ package com.sdl.weblocator.extjs;
 
 import com.extjs.selenium.button.Button;
 import com.extjs.selenium.conditions.MessageBoxSuccessCondition;
+import com.extjs.selenium.form.TextField;
+import com.extjs.selenium.panel.Panel;
 import com.extjs.selenium.window.MessageBox;
 import com.extjs.selenium.window.Window;
 import com.sdl.selenium.conditions.ConditionManager;
+import com.sdl.selenium.web.WebLocator;
 import com.sdl.weblocator.TestBase;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
@@ -18,6 +21,10 @@ public class FindElementTest extends TestBase {
     Button alertButton = new Button(elementWindow, "Alert");
     Button closeButton = new Button(elementWindow, "Close");
     Button findElementButton = new Button(null, "FindElement");
+    Panel findElementsAfterTimeoutFormPanel = new Panel("Find Elements after Timeout");
+    TextField timeOutTextField = new TextField(findElementsAfterTimeoutFormPanel, "Timeout:");
+    Button showButton = new Button(findElementsAfterTimeoutFormPanel, "Show");
+    Button showButton1 = new Button(findElementsAfterTimeoutFormPanel, "Show1").setRenderMillis(200);
 
 
     @Test
@@ -36,5 +43,27 @@ public class FindElementTest extends TestBase {
         conditionManager1.add(new MessageBoxSuccessCondition("Alert button was pressed"));
         Assert.assertTrue(conditionManager1.execute().isSuccess());
         MessageBox.pressOK();
+    }
+
+    @Test
+    public void findElementAfterTimeOut() {
+        long startMs = System.currentTimeMillis();
+        showButton1.waitToRender();
+        long endMs = System.currentTimeMillis();
+        logger.debug("wait1 = " + (endMs - startMs));
+    }
+
+    @Test
+    public void findElementAfterTimeOut2() {
+        WebLocator el = new WebLocator().setText("Timeout element in 3000");
+        showButton.click();
+
+        long startMs = System.currentTimeMillis();
+        Assert.assertTrue(el.waitToRender());
+        long endMs = System.currentTimeMillis();
+
+        MessageBox.pressOK();
+        timeOutTextField.setValue("3001");
+        logger.debug("wait1 = " + (endMs - startMs));
     }
 }
