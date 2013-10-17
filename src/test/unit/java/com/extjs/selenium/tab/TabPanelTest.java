5d8c0a6bd4dfabe64e86d69ebada5d55373db2a6
Viorel Culea <vculea@sdl.com>
2013-10-17 11:26:23 +0300

    improvement getItemPath for TabPanel

--- src/test/unit/java/com/extjs/selenium/tab/TabPanelTest.java
+++ src/test/unit/java/com/extjs/selenium/tab/TabPanelTest.java
@@ -11,7 +11,7 @@ public class TabPanelTest {
     @DataProvider
     public static Object[][] testConstructorPathDataProvider() {
         return new Object[][]{
-                {new TabPanel("Title"), "//*[contains(@class, 'x-tab-panel') and not(contains(@class, 'x-masked')) and count(*[contains(@class,'x-tab-panel-header')]//*[contains(@class, 'x-tab-strip-active')]//*[text()='Title']) > 0]//*[contains(@class, 'x-tab-panel-body')]/*[not(contains(@class, 'x-hide-display'))]"}
+                {new TabPanel("Title"), "//*[contains(@class, 'x-tab-panel') and not(contains(@class, 'x-masked')) and count(*[contains(@class,'x-tab-panel-header')]//*[contains(@class, 'x-tab-strip-active')]//*[text()='Title']) > 0]/*/*[contains(@class, 'x-tab-panel-body')]/*[not(contains(@class, 'x-hide-display'))]"}
         };
     }
 
