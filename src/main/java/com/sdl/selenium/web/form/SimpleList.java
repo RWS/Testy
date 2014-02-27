4c5a737fd12a6e00869aa2335151d6413124b8b5
Adela Pop <apop@sdl.com>
2014-02-27 12:07:34 +0200

    add SimpleList

---
+++ src/main/java/com/sdl/selenium/web/form/SimpleList.java
@@ -0,0 +1,67 @@
+package com.sdl.selenium.web.form;
+
+import com.sdl.selenium.web.IWebLocator;
+import com.sdl.selenium.web.WebLocator;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
+
+public class SimpleList extends WebLocator implements ICombo, IWebLocator {
+    private static final Logger logger = Logger.getLogger(SimpleList.class);
+
+    public SimpleList() {
+        setClassName("SimpleList");
+        setTag("select");
+    }
+
+    public SimpleList(WebLocator container) {
+        this();
+        setContainer(container);
+    }
+
+    public SimpleList(WebLocator container, String label) {
+        this(container);
+        setLabel(label);
+    }
+
+    @Override
+    public boolean select(String value) {
+        boolean selected = false;
+        if (ready()) {
+            new Select(currentElement).selectByVisibleText(value);
+            selected = true;
+        }
+        if (selected) {
+            logger.info("Set value(" + this + "): " + value);
+        }
+        return selected;
+    }
+
+    @Override
+    public boolean setValue(String value) {
+        return select(value);
+    }
+
+    @Override
+    public String getValue() {
+        String value = this.getAttribute("value");
+        return new WebLocator(this, "//option[contains(@value, '" + value + "')]").getHtmlText();
+    }
+
+    public boolean selectRows(String[] values) {
+        boolean select = false;
+        if (ready()) {
+            sendKeys(Keys.CONTROL, Keys.DOWN);
+            for (String value : values) {
+                WebLocator el = new WebLocator(this, "//*[contains(text(),'" + value + "')]");
+                select = el.click();
+                if (!select) {
+                    sendKeys(Keys.UP);
+                    return false;
+                }
+            }
+            sendKeys(Keys.UP);
+        }
+        return select;
+    }
+}
