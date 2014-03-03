71ba63756a043580c4c9bb6eb0d722fa6f384433
Culea Viorel <vculea@sdl.com>
2014-03-03 16:19:12 +0200

    getAttribute add in IWebLocator

--- src/main/java/com/sdl/selenium/web/IWebLocator.java
+++ src/main/java/com/sdl/selenium/web/IWebLocator.java
@@ -6,6 +6,18 @@ package com.sdl.selenium.web;
  */
 public interface IWebLocator {
 
+    String getAttributeId();
+
+    String getAttributeClass();
+
+    String getCurrentElementPath();
+
+    /**
+     * @param attribute e.g. class, id
+     * @return String attribute, if element not exist return null.
+     */
+    public String getAttribute(String attribute);
+
     boolean isElementPresent();
 
     int size();
