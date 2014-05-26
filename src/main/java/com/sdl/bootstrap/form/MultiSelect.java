cc0d78a74d0b7e65b34b2df02da9a05f3a3414fd
Culea Viorel <vculea@sdl.com>
2014-05-26 11:05:09 +0300

    MultiSelect.java

---
+++ src/main/java/com/sdl/bootstrap/form/MultiSelect.java
@@ -0,0 +1,11 @@
+package com.sdl.bootstrap.form;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vculea
+ * Date: 5/26/14
+ * Time: 11:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiSelect {
+}
