95a8c1b490f1baadc9e208be983b5b80605e7ac6
Culea Viorel <culeaviorel@gmail.com>
2015-03-04 21:05:28 +0200

    improvement pom.xml

--- src/test/functional/java/com/sdl/weblocator/bootstrap/form/MultiSelectTest.java
+++ src/test/functional/java/com/sdl/weblocator/bootstrap/form/MultiSelectTest.java
@@ -12,8 +12,8 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
 import static org.testng.Assert.assertTrue;
 
 public class MultiSelectTest extends TestBase {
