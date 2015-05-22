34bda5314f90c86105df30692286fc553df7b332
Culea Viorel <vculea@sdl.com>
2015-05-22 16:14:07 +0300

    don't use deprecated methods in tests

--- src/test/unit/java/com/sdl/selenium/extjs3/grid/GridCellTest.java
+++ src/test/unit/java/com/sdl/selenium/extjs3/grid/GridCellTest.java
@@ -15,14 +15,13 @@ public class GridCellTest {
                 {new GridCell(1).setContainer(grid),                     grid.getPath() + "//td[1]//*[contains(@class, 'x-grid3-cell-inner')]"},
                 {new GridCell("text", grid),                grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-cell-inner ') and contains(text(),'text')]"},
                 {new GridCell("text", SearchType.EQUALS),          "//*[contains(concat(' ', @class, ' '), ' x-grid3-cell-inner ') and text()='text']"},
-                {new GridCell(grid, "text", SearchType.EQUALS),          grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-cell-inner ') and text()='text']"},
-                {new GridCell(grid, 2, "text", SearchType.EQUALS),       grid.getPath() + "//td[2]//*[text()='text']"},
-                {new GridCell(grid, 2, "/text/exe", SearchType.CONTAINS_ALL),       grid.getPath() + "//td[2]//*[contains(text(),'text') and contains(text(),'exe')]"},
-                {new GridCell(grid, 2, "|text|exe", SearchType.CONTAINS_ANY),       grid.getPath() + "//td[2]//*[(contains(text(),'text') or contains(text(),'exe'))]"},
+                {new GridCell(2, "text", SearchType.EQUALS),       "//td[2]//*[text()='text']"},
+                {new GridCell(2, "/text/exe", SearchType.CONTAINS_ALL),       "//td[2]//*[contains(text(),'text') and contains(text(),'exe')]"},
+                {new GridCell(2, "|text|exe", SearchType.CONTAINS_ANY),       "//td[2]//*[(contains(text(),'text') or contains(text(),'exe'))]"},
                 {new GridCell(grid, "text", SearchType.CONTAINS),    grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-cell-inner ') and contains(text(),'text')]"},
                 {new GridCell(grid, "text", SearchType.STARTS_WITH), grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-cell-inner ') and starts-with(text(),'text')]"},
-                {new GridCell(grid.getRow(new GridCell(2, "text", SearchType.EQUALS))).setPosition(8).setText("Continue"), grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-row ') and not(contains(@class, 'x-grid3-row-checker')) and count(.//td[2]//*[text()='text']) > 0]//td[8]//*[contains(text(),'Continue')]"},
-                {new GridCell(grid.getRow(new GridCell(2, "text", SearchType.EQUALS))).setPosition(8).setText("Continue").setVisibility(true), grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-row ') and not(contains(@class, 'x-grid3-row-checker')) and count(.//td[2]//*[text()='text']) > 0]//td[8]//*[contains(text(),'Continue') and count(ancestor-or-self::*[contains(@style, 'display: none')]) = 0]"},
+                {new GridCell().setContainer(grid.getRow(new GridCell(2, "text", SearchType.EQUALS))).setPosition(8).setText("Continue"), grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-row ') and not(contains(@class, 'x-grid3-row-checker')) and count(.//td[2]//*[text()='text']) > 0]//td[8]//*[contains(text(),'Continue')]"},
+                {new GridCell().setContainer(grid.getRow(new GridCell(2, "text", SearchType.EQUALS))).setPosition(8).setText("Continue").setVisibility(true), grid.getPath() + "//*[contains(concat(' ', @class, ' '), ' x-grid3-row ') and not(contains(@class, 'x-grid3-row-checker')) and count(.//td[2]//*[text()='text']) > 0]//td[8]//*[contains(text(),'Continue') and count(ancestor-or-self::*[contains(@style, 'display: none')]) = 0]"},
         };
     }
 
