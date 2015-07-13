b3f50c1bb046cb640a23fb380ad9cf0814f941cb
Culea Viorel <vculea@sdl.com>
2015-07-13 11:49:09 +0300

    fix tests

--- src/main/java/com/sdl/selenium/utils/browsers/ChromeConfigReader.java
+++ src/main/java/com/sdl/selenium/utils/browsers/ChromeConfigReader.java
@@ -19,11 +19,8 @@ public class ChromeConfigReader extends AbstractBrowserConfigReader {
             "\n browser=chrome" +
             "\n browser.driver.path=src\\\\test\\\\resources\\\\drivers\\\\chromedriver.exe" +
             "\n browser.download.dir=src\\\\test\\\\resources\\\\download\\\\" +
-            "\n options.arguments=--lang=en --allow-running-insecure-content --enable-logging --v=1 --test-type --disable-popup-blocking, --disable-extensions-file-access-check" +
-//            "\n options.arguments=--lang=en --allow-running-insecure-content --enable-logging --v=1 --test-type --disable-popup-blocking, --disable-extensions-file-access-check --safebrowsing-disable-download-protection --disable-web-security" +
+            "\n options.arguments=--lang=en --allow-running-insecure-content --enable-logging --v=1 --test-type" +
             "\n options.experimental.profile.default_content_settings.multiple-automatic-downloads=1" +
-//            "\n options.experimental.profile.managed_default_content_settings.images=2" +
-//            "\n options.experimental.safebrowsing.enabled=0" +
             "\n options.experimental.download.prompt_for_download=1";
 
     public ChromeConfigReader() {
@@ -85,22 +82,8 @@ public class ChromeConfigReader extends AbstractBrowserConfigReader {
         if (!"".equals(downloadDir)) {
             prefs.put("download.default_directory", downloadDir);
         }
-//        prefs.put("download.directory_upgrade", true);
-//        prefs.put("download.extensions_to_open", "exe");
-        prefs.put("download.prompt_for_download", false);
-        prefs.put("safebrowsing.enabled", true);
         String arguments = getProperty("options.arguments");
         options.addArguments(arguments);
         options.setExperimentalOption("prefs", prefs);
-//        options.push('--ignore-certificate-errors')
-//        chromeSwitches.push('--disable-popup-blocking')
-//        chromeSwitches.push('--disable-extensions-file-access-check')
-//        chromeSwitches.push('--safebrowsing-disable-download-protection')
-        options.addArguments("--disable-improved-download-protection");
-        options.addArguments("--safebrowsing-disable-download-protection");
-        options.addArguments("--disable-backing-store-limit");
-        options.addArguments("--disable-prompt-on-repost");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-extensions-file-access-check");
     }
 }
