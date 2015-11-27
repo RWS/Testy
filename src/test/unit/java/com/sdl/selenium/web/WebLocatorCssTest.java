0a8a3dc7aece91abe87d13314b29d4a4d10c6ef4
vculea <vculea@sdl.com>
2015-11-27 09:38:02 +0200

    simply tests

--- src/test/unit/java/com/sdl/selenium/web/WebLocatorCssTest.java
+++ src/test/unit/java/com/sdl/selenium/web/WebLocatorCssTest.java
@@ -14,35 +14,13 @@ public class WebLocatorCssTest {
     public static Object[][] testConstructorCssSelectorDataProvider() {
         return new Object[][]{
                 {new WebLocator(), null},
-                {new WebLocator().setTag("td"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container).setText("Text"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container).setText("Text").setTitle("Title"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container).setText("Text").setTitle("Title").setRoot("./"), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container).setText("Text").setTitle("Title").setRoot("./").setResultIdx(2), null},
-                {new WebLocator().setTag("td").setBaseCls("BaseCls").setClasses("classes").setCls("Cls").setExcludeClasses("noCls").setId("Id").setLabel("Label").setLabelTag("info").setName("Name").setElPathSuffix("elPath", "@value='Test'").setVisibility(true).setStyle("display: block").setPosition(2).setContainer(container).setText("Text").setTitle("Title").setRoot("./").setResultIdx(2).setType("type"), null},
-
-                {new WebLocator("testcls"), null},
-                {new WebLocator(container), null},
-                {new WebLocator(container).setClasses("Cls"), null},
-                {new WebLocator("testcls", container), null},
-                {new WebLocator("text", "testcls", container), null},
-                {new WebLocator(container).setTag("textarea"), null},
-                {new WebLocator(container).setElCssSelector(".testcls").setTag("textarea"), ".testcls"},
-                {new WebLocator().setElPathSuffix("has-div", "count(div) > 0"), null},
-                {new WebLocator().setExcludeClasses("cls1", "cls2"), null},
+                {new WebLocator().setElCssSelector("div.error-msg"), "div.error-msg"},
+                {new WebLocator().setId("id"), null},
+                {new WebLocator().setTag("td"), "td"},
+                {new WebLocator().setCls("error-msg"), ".error-msg"}, //TODO see if supports only one class
+                {new WebLocator().setClasses("error-msg"), ".error-msg"},
+                {new WebLocator().setClasses("error-msg", "error"), ".error-msg.error"},
+                {new WebLocator().setName("error-msg"), ".error-msg.error"},
         };
     }
 
