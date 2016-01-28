12cb94816fabdf3b9ad833550f17dbd75746ec77
vculea <vculea@sdl.com>
2016-01-28 14:00:54 +0200

    add ieCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);

--- src/main/java/com/sdl/selenium/utils/browsers/IExplorerConfigReader.java
+++ src/main/java/com/sdl/selenium/utils/browsers/IExplorerConfigReader.java
@@ -2,13 +2,18 @@ package com.sdl.selenium.utils.browsers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class IExplorerConfigReader extends AbstractBrowserConfigReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IExplorerConfigReader.class);
 
-    private static final String DEFAULT_CONFIG = ""+
+    private static final String DEFAULT_CONFIG = "" +
             "\n browser=iexplore" +
             "\n browser.driver.path=src\\\\test\\\\resources\\\\drivers\\\\IEDriverServer.exe";
 
@@ -24,10 +29,12 @@ public class IExplorerConfigReader extends AbstractBrowserConfigReader {
     public WebDriver createDriver() throws IOException {
         DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
         ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        ieCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         String driverPath = getProperty("browser.driver.path");
         if (!"".equals(driverPath)) {
             System.setProperty("webdriver.ie.driver", driverPath);
         }
+        //setExtraProperty();
         return new InternetExplorerDriver(ieCapabilities);
     }
 
@@ -40,5 +47,14 @@ public class IExplorerConfigReader extends AbstractBrowserConfigReader {
     public String getDownloadPath() {
         //http://stackoverflow.com/questions/18510965/how-to-set-internetexplorerdriver-download-directory
         return "";
+    }
+
+    private void setExtraProperty() {
+        for (Map.Entry<Object, Object> entry : entrySet()) {
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
+            overWriteWebLocatorConfig(key, value);
+        }
+        LOGGER.info("The properties was load with success: {}", toString());
     }
 }
