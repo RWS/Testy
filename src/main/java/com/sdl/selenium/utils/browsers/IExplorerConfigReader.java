693647fb79b0ba05faaf2a44423f9b9377d836f6
vculea <vculea@sdl.com>
2016-02-10 13:48:29 +0200

    improvement IExplorerConfigReader

--- src/main/java/com/sdl/selenium/utils/browsers/IExplorerConfigReader.java
+++ src/main/java/com/sdl/selenium/utils/browsers/IExplorerConfigReader.java
@@ -2,19 +2,21 @@ package com.sdl.selenium.utils.browsers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class IExplorerConfigReader extends AbstractBrowserConfigReader {
     private static final Logger LOGGER = LoggerFactory.getLogger(IExplorerConfigReader.class);
 
     private static final String DEFAULT_CONFIG = "" +
             "\n browser=iexplore" +
-            "\n browser.driver.path=src\\\\test\\\\resources\\\\drivers\\\\IEDriverServer.exe";
+            "\n browser.driver.path=src\\\\test\\\\resources\\\\drivers\\\\IEDriverServer.exe" +
+            "\n desired.capabilities.ignoreProtectedModeSettings=true" +
+            "\n desired.capabilities.javascriptEnabled=true";
 
     public IExplorerConfigReader() {
         this(null);
@@ -26,14 +28,15 @@ public class IExplorerConfigReader extends AbstractBrowserConfigReader {
 
     @Override
     public WebDriver createDriver() throws IOException {
-        DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-        ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-        ieCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+//        capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+//        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        setCapabilities(capabilities);
         String driverPath = getProperty("browser.driver.path");
         if (!"".equals(driverPath)) {
             System.setProperty("webdriver.ie.driver", driverPath);
         }
-        return new InternetExplorerDriver(ieCapabilities);
+        return new InternetExplorerDriver(capabilities);
     }
 
     @Override
@@ -45,5 +48,28 @@ public class IExplorerConfigReader extends AbstractBrowserConfigReader {
     public String getDownloadPath() {
         //http://stackoverflow.com/questions/18510965/how-to-set-internetexplorerdriver-download-directory
         return "";
+    }
+
+    private void setCapabilities(DesiredCapabilities capabilities) {
+        for (Map.Entry<Object, Object> entry : entrySet()) {
+            String key = (String) entry.getKey();
+            if (key.startsWith("desired.capabilities.")) {
+                String preferenceKey = key.substring(21);
+                String value = (String) entry.getValue();
+
+                if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false")) {
+                    boolean aBoolean = Boolean.valueOf(value);
+                    capabilities.setCapability(preferenceKey, aBoolean);
+                } else {
+                    try {
+                        int intValue = Integer.parseInt(value);
+                        capabilities.setCapability(preferenceKey, intValue);
+                    } catch (NumberFormatException e) {
+                        capabilities.setCapability(preferenceKey, value);
+                    }
+                }
+            }
+        }
+        LOGGER.info("The properties was load with success: {}", toString());
     }
 }
