fb9e96fcd690fa5ba257d15bbdfeb9ad0e47ddf3
vculea <vculea@sdl.com>
2016-12-22 16:37:31 +0200

    add TimeField and improvement Tree

--- src/main/java/com/sdl/selenium/extjs6/tree/Tree.java
+++ src/main/java/com/sdl/selenium/extjs6/tree/Tree.java
@@ -1,6 +1,8 @@
 package com.sdl.selenium.extjs6.tree;
 
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,14 +23,15 @@ public class Tree extends WebLocator {
         boolean selected = false;
         for (String node : nodes) {
             WebLocator textEl = new WebLocator().setText(node);
-            WebLocator nodeEl = new WebLocator(this).setClasses("x-grid-item").setChildNodes(textEl);
+            Table nodeEl = new Table(this).setClasses("x-grid-item").setChildNodes(textEl).setVisibility(true);
+            Row row = nodeEl.getRow(1).setClasses("x-grid-row");
             WebLocator expanderEl = new WebLocator(nodeEl).setClasses("x-tree-expander");
-            WebLocator expandedEl = new WebLocator(nodeEl).setClasses("x-tree-icon-parent-expanded");
-            if (nodeEl.ready() && !expandedEl.isElementPresent() && expanderEl.isElementPresent()) {
+            if (nodeEl.ready() && !(row.getAttributeClass().contains("x-grid-tree-node-expanded") || row.getAttributeClass().contains("x-grid-tree-node-leaf"))) {
                 expanderEl.click();
             } else {
-                WebLocator checkTree = new WebLocator(nodeEl).setClasses(" x-tree-checkbox");
-                selected = checkTree.click();
+                WebLocator checkTree = new WebLocator(nodeEl).setClasses("x-tree-checkbox");
+                WebLocator nodeTree = new WebLocator(nodeEl).setClasses("x-tree-node-text");
+                selected = checkTree.isElementPresent() ? checkTree.click() : nodeTree.click();
             }
         }
         return selected;
