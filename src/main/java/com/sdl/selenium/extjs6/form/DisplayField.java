11064dcd856e4f65e5d3a2335d0ad191251ece5f
vculea <vculea@sdl.com>
2017-01-04 12:48:41 +0200

    add DisplayField

---
+++ src/main/java/com/sdl/selenium/extjs6/form/DisplayField.java
@@ -0,0 +1,25 @@
+package com.sdl.selenium.extjs6.form;
+
+import com.sdl.selenium.web.WebLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DisplayField extends WebLocator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DisplayField.class);
+
+    public DisplayField() {
+        withClassName("DisplayField");
+        withBaseCls("x-form-display-field");
+        withTag("div");
+    }
+
+    public DisplayField(WebLocator container) {
+        this();
+        withContainer(container);
+    }
+
+    public DisplayField(WebLocator container, String label) {
+        this(container);
+        withLabel(label);
+    }
+}
