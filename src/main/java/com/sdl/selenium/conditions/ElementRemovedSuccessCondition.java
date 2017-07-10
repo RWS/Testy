5b1da7eef8366790be9621f18349091cd7ef6cce
vculea <vculea@sdl.com>
2017-07-10 15:29:34 +0300

    ElementRemovedSuccessCondition implements RenderCondition

--- src/main/java/com/sdl/selenium/conditions/ElementRemovedSuccessCondition.java
+++ src/main/java/com/sdl/selenium/conditions/ElementRemovedSuccessCondition.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 // TODO is possible that ElementRemovedSuccessCondition is executed because some error messages have been arrived
 // think about some solutions to fix this.
-public class ElementRemovedSuccessCondition extends SuccessCondition {
+public class ElementRemovedSuccessCondition extends SuccessCondition implements RenderCondition {
     private static final Logger LOGGER = LoggerFactory.getLogger(ElementRemovedSuccessCondition.class);
 
     private WebLocator component;
@@ -15,18 +15,32 @@ public class ElementRemovedSuccessCondition extends SuccessCondition {
         super(message);
     }
 
-    public ElementRemovedSuccessCondition(WebLocator component){
+    public ElementRemovedSuccessCondition(WebLocator component) {
         this(component.toString());
         this.component = component;
     }
 
-    public ElementRemovedSuccessCondition(WebLocator component, int priority){
+    public ElementRemovedSuccessCondition(WebLocator component, int priority) {
         this(component);
         setPriority(priority);
     }
 
     public boolean execute() {
         return !component.isElementPresent();
+    }
+
+    @Override
+    public WebLocator getComponent() {
+        return component;
+    }
+
+    /**
+     *
+     * @return component.getText()
+     */
+    @Override
+    public String getResultMessage() {
+        return component.getText();
     }
 
     public String toString() {
