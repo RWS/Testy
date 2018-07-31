80debda5da74ffc93bf7c54c519d1eb800b97642
vculea <vculea@sdl.com>
2018-07-31 16:28:33 +0300

    add sleep time before click on close notification

--- src/main/java/com/sdl/selenium/extjs6/notification/Notification.java
+++ src/main/java/com/sdl/selenium/extjs6/notification/Notification.java
@@ -2,7 +2,7 @@ package com.sdl.selenium.extjs6.notification;
 
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
-import com.sdl.selenium.web.utils.RetryUtils;
+import com.sdl.selenium.web.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +28,7 @@ public class Notification extends WebLocator {
 
     public void close() {
         WebLocator close = new WebLocator(this).setClasses("x-tool-close");
-        RetryUtils.retry(2, close::click);
+        Utils.sleep(500);
+        close.click();
     }
 }
