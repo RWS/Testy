4c1b04018fdacbe1ba4906750051b20b61243541
Lilla Szasz <lszasz@sdl.com>
2020-05-05 21:33:15 +0300

    adding select methods

--- src/main/java/com/sdl/selenium/extjs6/form/Combo.java
+++ src/main/java/com/sdl/selenium/extjs6/form/Combo.java
@@ -1,14 +1,24 @@
 package com.sdl.selenium.extjs6.form;
 
+import com.sdl.selenium.WebLocatorUtils;
+import com.sdl.selenium.extjs6.panel.Pagination;
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.form.Field;
 import com.sdl.selenium.web.form.ICombo;
+import com.sdl.selenium.web.utils.Utils;
+import org.slf4j.Logger;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public abstract class Combo extends Field implements ICombo {
+
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Combo.class);
+    private Pagination paginationEl = new Pagination(getBoundList()).setRenderMillis(300);
 
     protected WebLocator getComboEl(String value, long optionRenderMillis, SearchType... searchType) {
         return new WebLocator(getBoundList()).setTag("li").setText(value, searchType).setRenderMillis(optionRenderMillis).setInfoMessage(value);
@@ -39,5 +49,58 @@ public abstract class Combo extends Field implements ICombo {
 
     public boolean collapse() {
         return "false".equals(getAttribute("aria-expanded")) || clickIcon("trigger");
+    }
+
+    public boolean select(String value) {
+        return select(value, SearchType.EQUALS);
+    }
+
+    public boolean select(String value, SearchType... searchType) {
+        boolean selected = doSelect(value, 300L, false, searchType);
+        assertThat("Could not selected value on: " + this, selected);
+        return selected;
+    }
+
+    /**
+     * @param value              value
+     * @param optionRenderMillis eg. 300ms
+     * @param pagination         true | false
+     * @param searchType         use {@link SearchType}
+     * @return true if value was selected
+     */
+    public boolean doSelect(String value, long optionRenderMillis, boolean pagination, SearchType... searchType) {
+        if (value.equals(getValue())) {
+            return true;
+        }
+//        waitToRender(300L);
+        boolean selected;
+        String info = toString();
+        WebLocator option = getComboEl(value, optionRenderMillis, searchType).setVisibility(true);
+        boolean trigger = expand();
+        if (trigger) {
+            if (pagination) {
+                do {
+                    if (selected = option.doClick()) {
+                        break;
+                    }
+                } while (paginationEl.goToNextPage());
+            } else {
+                selected = option.doClick();
+                if (!selected && option.isPresent()) {
+                    WebLocatorUtils.scrollToWebLocator(option);
+                    selected = option.doClick();
+                }
+            }
+            if (selected) {
+                log.info("Set value(" + info + "): " + value);
+                Utils.sleep(20);
+                return true;
+            }
+            collapse();
+            log.debug("(" + info + ") The option '" + value + "' could not be located. " + option.getXPath());
+        } else {
+            log.debug("(" + info + ") The combo or arrow could not be located.");
+        }
+        return false;
     }
 }
