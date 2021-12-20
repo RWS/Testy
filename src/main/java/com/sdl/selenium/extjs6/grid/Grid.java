d5b8e506cc11b8c521343c3335d13127e15a3bdc
vculea <vculea@sdl.com>
2021-12-20 18:36:29 +0200

    improvement locked grid

--- src/main/java/com/sdl/selenium/extjs6/grid/Grid.java
+++ src/main/java/com/sdl/selenium/extjs6/grid/Grid.java
@@ -250,7 +250,7 @@ public class Grid extends Table implements Scrollable, XTool {
                     List<String> list = new ArrayList<>();
                     Row row = new Row(containerLocked).setTag("tr").setResultIdx(i);
                     for (int j : firstColumns) {
-                        Cell cell = new Cell(row, true, j);
+                        Cell cell = new Cell(row, false, j);
                         String text;
                         if (predicate.test(j)) {
                             text = function.apply(cell);
@@ -262,7 +262,7 @@ public class Grid extends Table implements Scrollable, XTool {
                     WebLocator containerUnLocked = new WebLocator(this).setClasses("x-grid-scrollbar-clipper").setExcludeClasses("x-grid-scrollbar-clipper-locked");
                     row = new Row(containerUnLocked).setTag("tr").setResultIdx(i);
                     for (int j : secondColumns) {
-                        Cell cell = new Cell(row, true,j - cells);
+                        Cell cell = new Cell(row, false,j - cells);
                         String text;
                         if (predicate.test(j)) {
                             text = function.apply(cell);
