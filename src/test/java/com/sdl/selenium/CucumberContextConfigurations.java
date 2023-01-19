9a3c9ec4cff520e3aaa08e24b26b9877253063d2
Lilla Szasz <lszasz@sdl.com>
2023-01-19 11:31:38 +0200

    fix spring boot context configuration

--- src/test/java/com/sdl/selenium/CucumberContextConfigurations.java
+++ src/test/java/com/sdl/selenium/CucumberContextConfigurations.java
@@ -1,9 +1,9 @@
 package com.sdl.selenium;
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
-@SpringBootTest(classes = TestConfig.class)
+@ContextConfiguration(locations = {"classpath:cucumber.xml"})
 public class CucumberContextConfigurations {
 }
