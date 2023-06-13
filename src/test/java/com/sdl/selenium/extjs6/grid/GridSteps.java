9a1f89d9c8f612c3122c0ee7e56b142feb87e4c6
vculea <vculea@sdl.com>
2023-06-13 21:43:59 +0300

    improvement getParallelCellsText(int... excludedColumns) method

--- src/test/java/com/sdl/selenium/extjs6/grid/GridSteps.java
+++ src/test/java/com/sdl/selenium/extjs6/grid/GridSteps.java
@@ -2,17 +2,14 @@ package com.sdl.selenium.extjs6.grid;
 
 import com.sdl.selenium.InputData;
 import com.sdl.selenium.TestBase;
-import com.sdl.selenium.utils.Storage;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.utils.Utils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 
 import static com.sdl.selenium.utils.MatcherAssertList.assertThatList;
@@ -20,15 +17,10 @@ import static org.hamcrest.Matchers.contains;
 
 @Slf4j
 public class GridSteps extends TestBase {
-
-    @Autowired
-    private Storage storage;
     private final Grid grid = new Grid().setTitle("Cell Editing Plants");
-    private final Grid employees = new Grid().setTitle("Row Editing Employees");
 
     @Given("I open extjs6 app and add {string} path")
     public void iOpenExtjs6AppAndAddPath(String path) {
-        storage.set("email", "nige.white@sentcha.com");
         String url = InputData.EXTJS_EXAMPLE_URL + path;
         log.info("Url: {}", url);
         driver.get(url);
@@ -46,27 +38,18 @@ public class GridSteps extends TestBase {
         assertThatList("Actual values: ", cellsText, contains(values.toArray()));
     }
 
-    private Function<Cell, String> getBooleanValue() {
-        return cell -> {
-            Map<String, String[]> templatesValues = cell.getPathBuilder().getTemplatesValues();
-            String[] tagAndPositions = templatesValues.get("tagAndPosition");
-            List<String> positions = List.of(tagAndPositions);
-            if (positions.size() == 1) {
-                String position = positions.get(0);
-                if (position.equals("5")) {
-                    WebLocator check = new WebLocator(cell).setClasses("x-grid-checkcolumn");
-                    boolean checked = check.getAttributeClass().contains("x-grid-checkcolumn-checked");
-                    return checked ? "true" : "false";
-                }
-            }
-            return null;
+    public static Function<Cell, String> getBooleanValue() {
+        return f -> {
+            WebLocator check = new WebLocator(f).setClasses("x-grid-checkcolumn");
+            boolean checked = check.getAttributeClass().contains("x-grid-checkcolumn-checked");
+            return checked ? "true" : "false";
         };
     }
 
     @Then("I verify parallel if grid has values:")
     public void iVerifyParallelIfGridHasValues(List<List<String>> values) {
         long startMs = System.currentTimeMillis();
-        List<List<String>> cellsText = employees.getParallelValues(t -> t == 5, getBooleanValue());
+        List<List<String>> cellsText = grid.getParallelValues(t -> t == 5, getBooleanValue(), 6);
         long endMs = System.currentTimeMillis();
         long rez = endMs - startMs;
         log.debug("performance took {} ms", rez);
