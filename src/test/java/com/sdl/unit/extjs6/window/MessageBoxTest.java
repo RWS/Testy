8690934262a9a653b98ec1ad1ae9cf6d520de910
vculea <vculea@sdl.com>
2023-08-17 16:22:08 +0300

    revert improvement MessageBox.java

--- src/test/java/com/sdl/unit/extjs6/window/MessageBoxTest.java
+++ src/test/java/com/sdl/unit/extjs6/window/MessageBoxTest.java
@@ -14,8 +14,8 @@ public class MessageBoxTest {
     public static Object[][] testConstructorPathDataProvider() {
         return new Object[][]{
                 {new MessageBox("Title"), "//*[contains(concat(' ', @class, ' '), ' x-message-box ') and not(contains(@class, 'x-hidden-offsets')) and count(.//*[contains(concat(' ', @class, ' '), ' x-header ')]//*[normalize-space(text())='Title']) > 0]"},
-                {new MessageBox("Title", "Message", SearchType.DEEP_CHILD_NODE_OR_SELF),  "//*[contains(concat(' ', @class, ' '), ' x-message-box ') and not(contains(@class, 'x-hidden-offsets')) and count(.//*[contains(concat(' ', @class, ' '), ' x-header ')]//*[normalize-space(text())='Title']) > 0 and count(//*[contains(concat(' ', @class, ' '), ' x-window-body ') and not(contains(@class, 'x-window-body-fullscreen'))]//*[(contains(.,'Message') or count(*//text()[contains(.,'Message')]) > 0)]) > 0]"},
-                {new MessageBox("Title", "Message"), "//*[contains(concat(' ', @class, ' '), ' x-message-box ') and not(contains(@class, 'x-hidden-offsets')) and count(.//*[contains(concat(' ', @class, ' '), ' x-header ')]//*[normalize-space(text())='Title']) > 0 and count(//*[contains(concat(' ', @class, ' '), ' x-window-body ') and not(contains(@class, 'x-window-body-fullscreen'))]//*[contains(text(),'Message')]) > 0]"},
+                {new MessageBox("Title", "Message", SearchType.DEEP_CHILD_NODE_OR_SELF),  "//*[contains(concat(' ', @class, ' '), ' x-message-box ') and not(contains(@class, 'x-hidden-offsets')) and count(.//*[contains(concat(' ', @class, ' '), ' x-header ')]//*[normalize-space(text())='Title']) > 0 and count(//*[contains(concat(' ', @class, ' '), ' x-window-body ')]//*[(contains(.,'Message') or count(*//text()[contains(.,'Message')]) > 0)]) > 0]"},
+                {new MessageBox("Title", "Message"), "//*[contains(concat(' ', @class, ' '), ' x-message-box ') and not(contains(@class, 'x-hidden-offsets')) and count(.//*[contains(concat(' ', @class, ' '), ' x-header ')]//*[normalize-space(text())='Title']) > 0 and count(//*[contains(concat(' ', @class, ' '), ' x-window-body ')]//*[contains(text(),'Message')]) > 0]"},
         };
     }
 
