efbbc104632fc8724af664e1dc63d5ed6890dfaa
vculea <vculea@sdl.com>
2024-10-09 09:38:33 +0300

    added do... for all options

--- src/main/java/com/sdl/selenium/extjs4/window/XTool.java
+++ src/main/java/com/sdl/selenium/extjs4/window/XTool.java
@@ -17,6 +17,11 @@ public interface XTool {
         return toolElement.click();
     }
 
+    default boolean doClickOnTool(String suffix) {
+        WebLocator toolElement = getToolEl(suffix).setVisibility(true);
+        return toolElement.doClick();
+    }
+
     default WebLocator getToolEl(String suffix) {
         return new WebLocator(getView()).setClasses("x-tool-" + suffix);
     }
@@ -25,31 +30,63 @@ public interface XTool {
         return clickOnTool("close");
     }
 
+    default boolean doClose() {
+        return doClickOnTool("close");
+    }
+
     default boolean maximize() {
         return clickOnTool("maximize");
+    }
+
+    default boolean doMaximize() {
+        return doClickOnTool("maximize");
     }
 
     default boolean restore() {
         return clickOnTool("restore");
     }
 
+    default boolean doRestore() {
+        return doClickOnTool("restore");
+    }
+
     default boolean minimize() {
         return clickOnTool("minimize");
+    }
+
+    default boolean doMinimize() {
+        return doClickOnTool("minimize");
     }
 
     default boolean toggle() {
         return clickOnTool("toggle");
     }
 
+    default boolean doToggle() {
+        return doClickOnTool("toggle");
+    }
+
     default boolean plus() {
         return clickOnTool("plus");
+    }
+
+    default boolean doPlus() {
+        return doClickOnTool("plus");
     }
 
     default boolean collapse() {
         return clickOnTool("collapse");
     }
 
+    default boolean doCollapse() {
+        return doClickOnTool("collapse");
+    }
+
     default boolean expand() {
         return clickOnTool("expand");
+    }
+
+    default boolean doExpand() {
+        return doClickOnTool("expand");
     }
 }
