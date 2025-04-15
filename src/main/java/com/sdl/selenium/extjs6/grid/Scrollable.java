c2aba8acfc0fcb2f16343afd0786ff248465ab48
vculea <vculea@sdl.com>
2025-04-15 12:50:41 +0300

    added scrollBy(int x, int y) method in Scrollable

--- src/main/java/com/sdl/selenium/extjs6/grid/Scrollable.java
+++ src/main/java/com/sdl/selenium/extjs6/grid/Scrollable.java
@@ -127,7 +127,9 @@ public interface Scrollable extends IWebLocator {
     default boolean scrollBy(int x, int y) {
         String id = getAttributeId();
         if (!Strings.isNullOrEmpty(id)) {
-            String script = "return (function(c){ c." + getView() + ".scrollable._scrollElement.dom.scrollBy(" + x + "," + y + ");return true}return false})(window.Ext.getCmp('" + id + "'))";
+            String script = "return (function(c)" +
+                    "{c." + getView() + ".scrollable._scrollElement.dom.scrollBy(" + x + "," + y + ");return true" +
+                    "})(window.Ext.getCmp('" + id + "'))";
             return (Boolean) WebLocatorUtils.doExecuteScript(script);
         }
         return false;
