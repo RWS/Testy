9f80ea6f62c71546a11d72fb85e55e010747df2d
vculea <vculea@sdl.com>
2025-07-07 16:22:53 +0300

    added selected(String value, Duration duration, SearchType... searchType) method in Combo

--- src/main/java/com/sdl/selenium/extjs6/form/Combo.java
+++ src/main/java/com/sdl/selenium/extjs6/form/Combo.java
@@ -25,21 +25,49 @@ public abstract class Combo extends Field implements ICombo {
     private static final Logger log = LogManager.getLogger(Combo.class);
     private final Pagination paginationEl = new Pagination(getBoundList()).setRender(Duration.ofMillis(300));
 
+    /**
+     * Returns the combo element (option) matching the given value and search type, waiting for it to render.
+     *
+     * @param value              the value to search for in the combo box
+     * @param optionRenderMillis the maximum time to wait for the option to render, in milliseconds
+     * @param searchType         the search strategy to use (e.g., EQUALS, CONTAINS)
+     * @return the WebLocator representing the combo option
+     * @deprecated Use {@link #getComboEl(String, Duration, SearchType...)} instead.
+     */
     @Deprecated
     protected WebLocator getComboEl(String value, long optionRenderMillis, SearchType... searchType) {
         return getComboEl(value, Duration.ofMillis(optionRenderMillis), searchType);
     }
 
+    /**
+     * Returns the combo element (option) matching the given value and search type, waiting for it to render.
+     *
+     * @param value      the value to search for in the combo box
+     * @param duration   the maximum time to wait for the option to render
+     * @param searchType the search strategy to use (e.g., EQUALS, CONTAINS)
+     * @param <T>        the type of WebLocator
+     * @return the WebLocator representing the combo option
+     */
     protected <T extends WebLocator> T getComboEl(String value, Duration duration, SearchType... searchType) {
         return new WebLocator(getBoundList()).setTag("li").setText(value, searchType).setRender(duration).setInfoMessage(value);
     }
 
+    /**
+     * Gets the current value selected in the combo box.
+     *
+     * @return the selected value as a String
+     */
     @Override
     public String getValue() {
         ready();
         return executor.getValue(this);
     }
 
+    /**
+     * Retrieves all values from the combo box as a list of strings.
+     *
+     * @return a list of all values in the combo box
+     */
     public List<String> getAllValues() {
         waitToRender(Duration.ofMillis(300L));
         expand();
@@ -56,6 +84,13 @@ public abstract class Combo extends Field implements ICombo {
         return result;
     }
 
+    /**
+     * Retrieves all values from the combo box and maps them to objects of the specified type.
+     *
+     * @param type the class type to map the values to
+     * @param <V>  the type of the objects in the returned list
+     * @return a list of objects of type V, or null if values are not available
+     */
     public <V> List<V> getAllValues(Class<V> type) {
         List<String> values = getAllValues();
         if (values == null) {
@@ -93,42 +128,74 @@ public abstract class Combo extends Field implements ICombo {
         }).collect(Collectors.toList());
     }
 
+    /**
+     * Expands the combo box dropdown if it is not already expanded.
+     *
+     * @return true if the combo box is expanded after the call, false otherwise
+     */
     public boolean expand() {
         return "true".equals(getAttribute("aria-expanded")) || clickIcon("trigger");
     }
 
+    /**
+     * Collapses the combo box dropdown if it is not already collapsed.
+     *
+     * @return true if the combo box is collapsed after the call, false otherwise
+     */
     public boolean collapse() {
         return "false".equals(getAttribute("aria-expanded")) || clickIcon("trigger");
     }
 
+    /**
+     * Selects the specified value in the combo box using the EQUALS search type.
+     *
+     * @param value the value to select in the combo box
+     * @return true if the value is successfully selected, false otherwise
+     */
     public boolean select(String value) {
         return select(value, SearchType.EQUALS);
     }
 
+    /**
+     * Selects the specified value in the combo box using the provided search type(s).
+     *
+     * @param value      the value to select in the combo box
+     * @param searchType the search strategy to use for finding the value (e.g., EQUALS, CONTAINS)
+     * @return true if the value is successfully selected, false otherwise
+     */
     public boolean select(String value, SearchType... searchType) {
-        boolean selected = doSelect(value, 300L, false, searchType);
+        boolean selected = doSelect(value, Duration.ofMillis(300), false, searchType);
         assertThat("Could not selected value on: " + this, selected);
         return selected;
     }
 
+    /**
+     * Selects the specified value in the combo box and verifies if it is selected.
+     * Uses a default timeout of 300 milliseconds for the selection operation.
+     *
+     * @param value      the value to select in the combo box
+     * @param searchType the search strategy to use for finding the value (e.g., EQUALS, CONTAINS)
+     * @return true if the value is successfully selected, false otherwise
+     */
     public boolean selected(String value, SearchType... searchType) {
-        Boolean selected = RetryUtils.retry(5, () -> {
-            doSelect(value, 300L, false, searchType);
-            return getValue().equals(value);
-        });
-        return selected;
+        return selected(value, Duration.ofMillis(300L), searchType);
     }
 
     /**
-     * @param value              value
-     * @param optionRenderMillis eg. 300ms
-     * @param pagination         true | false
-     * @param searchType         use {@link SearchType}
-     * @return true if value was selected
+     * Selects the specified value in the combo box and verifies if it is selected within the given duration.
+     * The method retries the selection up to 5 times if the value is not immediately selected.
+     *
+     * @param value      the value to select in the combo box
+     * @param duration   the maximum time to wait for the option to be rendered and selected
+     * @param searchType the search strategy to use for finding the value (e.g., EQUALS, CONTAINS)
+     * @return true if the value is successfully selected, false otherwise
      */
-    @Deprecated
-    public boolean doSelect(String value, long optionRenderMillis, boolean pagination, SearchType... searchType) {
-        return doSelect(value, Duration.ofMillis(optionRenderMillis), pagination, searchType);
+    public boolean selected(String value, Duration duration, SearchType... searchType) {
+        Boolean selected = RetryUtils.retry(5, () -> {
+            doSelect(value, duration, false, searchType);
+            return getValue().equals(value);
+        });
+        return selected;
     }
 
     /**
@@ -157,15 +224,13 @@ public abstract class Combo extends Field implements ICombo {
             } else {
                 selected = option.doClick();
                 if (!selected && option.isPresent()) {
-//                    WebLocatorUtils.scrollToWebLocator(option);
-//                    WebLocatorUtils.doExecuteScript("arguments[0].scrollIntoViewIfNeeded(false);", option.getWebElement());
                     option.scrollIntoView(Go.NEAREST);
                     selected = option.doClick();
                 }
             }
             if (selected) {
                 log.info("Set value({}): {}", info, value);
-                Utils.sleep(20);
+                Utils.sleep(10);
                 return true;
             }
             collapse();
--- src/main/java/com/sdl/selenium/extjs6/form/ComboBox.java
+++ src/main/java/com/sdl/selenium/extjs6/form/ComboBox.java
@@ -1,7 +1,7 @@
 package com.sdl.selenium.extjs6.form;
 
+import com.sdl.selenium.Go;
 import com.sdl.selenium.WebLocatorUtils;
-import com.sdl.selenium.extjs6.grid.Row;
 import com.sdl.selenium.extjs6.panel.Pagination;
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
@@ -20,20 +20,35 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Getter
 public class ComboBox extends Combo {
-    private static final Logger log = LogManager.getLogger(Row.class);
+    private static final Logger log = LogManager.getLogger(ComboBox.class);
     private final Pagination paginationEl = new Pagination(getBoundList()).setRender(Duration.ofMillis(300));
 
+    /**
+     * Default constructor for ComboBox. Sets up the class name, base class, and tag for the input element.
+     */
     public ComboBox() {
         setClassName("ComboBox");
         setBaseCls("x-form-text");
         setTag("input");
     }
 
+    /**
+     * Constructor for ComboBox with a container.
+     *
+     * @param container the WebLocator container in which the ComboBox is located
+     */
     public ComboBox(WebLocator container) {
         this();
         setContainer(container);
     }
 
+    /**
+     * Constructor for ComboBox with a container, label, and search types.
+     *
+     * @param container   the WebLocator container in which the ComboBox is located
+     * @param label       the label of the ComboBox
+     * @param searchTypes the search strategies to use for finding the label
+     */
     public ComboBox(WebLocator container, String label, SearchType... searchTypes) {
         this(container);
         if (searchTypes.length == 0) {
@@ -47,22 +62,26 @@ public class ComboBox extends Combo {
     }
 
     /**
-     * @param value              value
-     * @param optionRenderMillis eg. 300ms
-     * @param pagination         true | false
-     * @param searchType         use {@link SearchType}
-     * @return true if value was selected
+     * Selects the specified value in the combo box, with a timeout in milliseconds and optional pagination.
+     *
+     * @param value              the value to select
+     * @param optionRenderMillis the maximum time to wait for the option to render, in milliseconds
+     * @param pagination         whether to use pagination when searching for the value
+     * @param searchType         the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
      */
     public boolean doSelect(String value, long optionRenderMillis, boolean pagination, SearchType... searchType) {
         return doSelect(value, Duration.ofMillis(optionRenderMillis), pagination, searchType);
     }
 
     /**
-     * @param value      value
-     * @param duration   eg. 300ms
-     * @param pagination true | false
-     * @param searchType use {@link SearchType}
-     * @return true if value was selected
+     * Selects the specified value in the combo box, with a timeout and optional pagination.
+     *
+     * @param value      the value to select
+     * @param duration   the maximum time to wait for the option to render
+     * @param pagination whether to use pagination when searching for the value
+     * @param searchType the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
      */
     public boolean doSelect(String value, Duration duration, boolean pagination, SearchType... searchType) {
         if (value.equals(getValue())) {
@@ -86,93 +105,174 @@ public class ComboBox extends Combo {
                     return doClick && !option.ready(Duration.ofMillis(200));
                 });
                 if (!selected && option.isPresent()) {
-                    WebLocatorUtils.doExecuteScript("arguments[0].scrollIntoViewIfNeeded(false);", option.getWebElement());
-                    String id = getBoundList().getAttributeId();
-                    scrollBack(id);
+//                    WebLocatorUtils.doExecuteScript("arguments[0].scrollIntoViewIfNeeded(false);", option.getWebElement());
+                    option.scrollIntoView(Go.NEAREST);
                     selected = option.doClick();
-                    if (!selected && option.isPresent()) {
-                        scrollDown(id);
-                        selected = option.doClick();
-                    }
                 }
             }
             if (selected) {
-                log.info("Set value(" + info + "): " + value);
+                log.info("Set value({}): {}", info, value);
                 Utils.sleep(20);
                 return true;
             }
             collapse();
-            log.debug("(" + info + ") The option '" + value + "' could not be located. " + option.getXPath());
+            log.debug("({}) The option '{}' could not be located. {}", info, value, option.getXPath());
         } else {
-            log.debug("(" + info + ") The combo or arrow could not be located.");
+            log.debug("({}) The combo or arrow could not be located.", info);
         }
         return false;
     }
 
+    /**
+     * Scrolls the combo box list up by 50 pixels.
+     *
+     * @param id the id of the ExtJS component
+     * @return true if the scroll position increased (scrolled up), false otherwise
+     *
+     * <p>Example usage:</p>
+     * <pre>
+     *     comboBox.scrollBack("boundlist-1234");
+     * </pre>
+     */
     public boolean scrollBack(String id) {
         String script = "return (function (c) {var top = c.scrollable._scrollElement.dom.scrollTop;c.scrollBy(0,-50);var topTemp = c.scrollable._scrollElement.dom.scrollTop;return top < topTemp;})(window.Ext.getCmp('" + id + "'))";
         return (Boolean) WebLocatorUtils.doExecuteScript(script);
     }
 
+    /**
+     * Scrolls the combo box list down by 50 pixels.
+     *
+     * @param id the id of the ExtJS component
+     * @return true if the scroll position reached the bottom, false otherwise
+     *
+     * <p>Example usage:</p>
+     * <pre>
+     *     comboBox.scrollDown("boundlist-1234");
+     * </pre>
+     */
     public boolean scrollDown(String id) {
         String script = "return (function (c) {var top = c.scrollable._scrollElement.dom.scrollTop;c.scrollBy(0,50);return Math.round(top) >= c.scrollable.getMaxPosition().y;})(window.Ext.getCmp('" + id + "'))";
         return (Boolean) WebLocatorUtils.doExecuteScript(script);
     }
 
-    @Deprecated
-    public boolean doSelect(String value, long optionRenderMillis, SearchType... searchType) {
-        return doSelect(value, optionRenderMillis, false, searchType);
-    }
-
+    /**
+     * Selects the specified value in the combo box with a timeout and search type(s).
+     *
+     * @param value      the value to select
+     * @param duration   the maximum time to wait for the option to render
+     * @param searchType the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean doSelect(String value, Duration duration, SearchType... searchType) {
         return doSelect(value, duration, false, searchType);
     }
 
+    /**
+     * Selects the specified value in the combo box using the provided search type(s) and a default timeout of 300ms.
+     *
+     * @param value      the value to select
+     * @param searchType the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean select(String value, SearchType... searchType) {
         return select(value, Duration.ofMillis(300), searchType);
     }
 
+    /**
+     * Selects the specified value in the combo box using a timeout in milliseconds and the default EQUALS search type.
+     *
+     * @param value              the value to select
+     * @param optionRenderMillis the maximum time to wait for the option to render, in milliseconds
+     * @return true if the value was successfully selected, false otherwise
+     * @deprecated Use {@link #select(String, Duration)} instead.
+     */
     @Deprecated
     public boolean select(String value, long optionRenderMillis) {
         return select(value, Duration.ofMillis(optionRenderMillis));
     }
 
+    /**
+     * Selects the specified value in the combo box using a timeout and the default EQUALS search type.
+     *
+     * @param value    the value to select
+     * @param duration the maximum time to wait for the option to render
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean select(String value, Duration duration) {
         return select(value, duration, false, SearchType.EQUALS);
     }
 
+    /**
+     * Selects the specified value in the combo box using the default timeout (300ms) and optional pagination.
+     *
+     * @param value      the value to select
+     * @param pagination whether to use pagination when searching for the value
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean select(String value, boolean pagination) {
         return select(value, Duration.ofMillis(300), pagination);
     }
 
-    @Deprecated
-    public boolean select(String value, long optionRenderMillis, boolean pagination) {
-        return select(value, Duration.ofMillis(optionRenderMillis), pagination);
-    }
-
+    /**
+     * Selects the specified value in the combo box using a timeout and optional pagination.
+     *
+     * @param value      the value to select
+     * @param duration   the maximum time to wait for the option to render
+     * @param pagination whether to use pagination when searching for the value
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean select(String value, Duration duration, boolean pagination) {
         boolean selected = doSelect(value, duration, pagination, SearchType.EQUALS);
         assertThat("Could not selected value on: " + this, selected);
         return selected;
     }
 
+    /**
+     * Selects the specified value in the combo box using a timeout and search type(s).
+     *
+     * @param value      the value to select
+     * @param duration   the maximum time to wait for the option to render
+     * @param searchType the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
+     */
     public boolean select(String value, Duration duration, SearchType... searchType) {
         return select(value, duration, false, searchType);
     }
 
-    @Deprecated
-    public boolean select(String value, long optionRenderMillis, boolean pagination, SearchType... searchType) {
-        boolean selected = doSelect(value, optionRenderMillis, pagination, searchType);
-        assertThat("Could not selected value on: " + this, selected);
-        return selected;
-    }
-
+    /**
+     * Selects the specified value in the combo box using a timeout, optional pagination, and search type(s).
+     *
+     * @param value      the value to select
+     * @param duration   the maximum time to wait for the option to render
+     * @param pagination whether to use pagination when searching for the value
+     * @param searchType the search strategy to use
+     * @return true if the value was successfully selected, false otherwise
+     *
+     * <p>Example usage:</p>
+     * <pre>
+     *     comboBox.select("Option 1", Duration.ofMillis(500), true, SearchType.EQUALS);
+     * </pre>
+     */
     public boolean select(String value, Duration duration, boolean pagination, SearchType... searchType) {
         boolean selected = doSelect(value, duration, pagination, searchType);
         assertThat("Could not selected value on: " + this, selected);
         return selected;
     }
 
+    /**
+     * Selects the icon element associated with a value in the combo box.
+     *
+     * @param value     the value to select
+     * @param iconClass the CSS class of the icon
+     * @param duration  the maximum time to wait for the option to render
+     * @param searchType the search strategy to use
+     * @return true if the icon was successfully clicked, false otherwise
+     *
+     * <p>Example usage:</p>
+     * <pre>
+     *     comboBox.doSelectIcon("Option 1", "icon-delete", Duration.ofMillis(300), SearchType.EQUALS);
+     * </pre>
+     */
     public boolean doSelectIcon(String value, String iconClass, Duration duration, SearchType... searchType) {
         boolean selected;
         String info = toString();
@@ -196,14 +296,5 @@ public class ComboBox extends Combo {
             log.debug("({}) The combo or arrow could not be located.", info);
         }
         return false;
-    }
-
-    @Override
-    public boolean select(String value) {
-        return select(value, SearchType.EQUALS);
-    }
-
-    public boolean setValue(String value) {
-        return executor.setValue(this, value);
     }
 }
